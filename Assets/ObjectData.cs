1b9c12cf08fb16f58338d99b4d5e38cddc78a54f
mvjj <michalvjj@gmail.com>
2015-05-31 18:30:40 +0200

    ObjectData

---
+++ Assets/ObjectData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ObjectData : IComparable<ObjectData> {
+
+	public Vector3 pos;
+	public Quaternion rot;
+	
+	public ObjectData(Vector3 _pos, Quaternion _rot)
+	{
+		pos = _pos;
+		rot = _rot;
+	}
+
+	public int CompareTo(ObjectData other)
+	{
+		if(other == null)
+		{
+			return 1;
+		}
+
+		return (int)pos.x - (int)other.pos.x;
+	}
+}
