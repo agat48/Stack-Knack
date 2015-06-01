c08864d4095a00673a8b240409ee4b88644d01d7
agat48 <agat.paciorek@gmail.com>
2015-06-01 02:27:09 +0200

    GUI + new look

---
+++ Assets/LoadLevel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour {
+
+	public void LoadLevelByNumber (int num) {
+		Application.LoadLevel("s" + num);
+	}
+	public void Back() {
+		Application.LoadLevel("menu_16x9");
+	}
+}
