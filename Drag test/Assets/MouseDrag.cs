9dcfe58d2c0c499b4738037503e6f0973f9358c2
agat48 <agat.paciorek@gmail.com>
2015-06-22 21:52:48 +0200

    Objects drag project

---
+++ Drag test/Assets/MouseDrag.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseDrag : MonoBehaviour {
+	
+	private float dist;
+	private Transform toDrag;
+	public static Transform activeTransform;
+	private bool dragging = false;
+	private Vector3 offset;
+	private GameObject activeObj;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		drag ();
+	}
+	void drag() {
+		
+		Vector3 v3;
+		if(Input.touchCount >0 && Input.GetTouch(0).phase == TouchPhase.Began)
+		{
+			RaycastHit hit = new RaycastHit();
+			Ray ray  = Camera.main.ScreenPointToRay(Input.GetTouch(0).position); 
+			if(Physics.Raycast(ray, out hit))
+			{
+
+				activeObj = hit.transform.gameObject;
+				activeObj.GetComponent<Rigidbody>().isKinematic = false;
+				{
+					toDrag = hit.transform;
+					dist = hit.transform.position.z - Camera.main.transform.position.z;
+					v3 = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, dist);
+					v3 = Camera.main.ScreenToWorldPoint(v3);
+					offset = toDrag.position - v3;
+					dragging = true;
+				}
+			}
+		}
+		if (Input.touchCount >0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+		{
+			if (dragging)
+			{
+				v3 = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, dist);
+				v3 = Camera.main.ScreenToWorldPoint(v3);
+				toDrag.position = v3 + offset;
+			}
+		}
+		if (Input.touchCount >0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+		{
+			dragging = false;
+			activeObj.GetComponent<Rigidbody>().isKinematic = true;
+		}
+	}
+}
+	
